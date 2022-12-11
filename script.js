bab93d0125bae44b711f8efe44fccd3fdbdea137
dev7355608 <dev7355608@gmail.com>
2022-12-11 16:46:53 +0100

    Patch with libWrapper if active

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
+const MODULE_ID = "manage-player-targets";
+
 Hooks.once("ready", () => {
     if (!game.user.isGM || game.settings.get("core", "noCanvas")) {
         return;
     }
 
-    const TARGET_CONTROL_ACTIVE = Symbol();
+    const TARGET_CONTROL_ACTIVE = Symbol(MODULE_ID);
 
-    TokenHUD.prototype.bind = (original => function () {
+    function TokenHUD_bind(wrapped, ...args) {
         this[TARGET_CONTROL_ACTIVE] = false;
 
-        return original.apply(this, arguments);
-    })(TokenHUD.prototype.bind);
+        return wrapped(...args);
+    }
 
-    TokenHUD.prototype._onToggleTarget = function (event) {
+    function TokenHUD_onToggleTarget(event) {
         event.preventDefault();
 
         const button = event.currentTarget;
         const active = this[TARGET_CONTROL_ACTIVE] = !this[TARGET_CONTROL_ACTIVE];
 
         button.classList.toggle("active", active);
-        button.querySelector(`.manage-player-targets.player-list`).classList.toggle("active", active);
-    };
+        button.querySelector(`.${MODULE_ID}.player-list`).classList.toggle("active", active);
+    }
+
+    if (game.modules.get("lib-wrapper")?.active) {
+        libWrapper.register(
+            MODULE_ID,
+            "TokenHUD.prototype.bind",
+            TokenHUD_bind,
+            libWrapper.WRAPPER
+        );
+
+        libWrapper.register(
+            MODULE_ID,
+            "TokenHUD.prototype._onToggleTarget",
+            TokenHUD_onToggleTarget,
+            libWrapper.OVERRIDE
+        );
+    } else {
+        TokenHUD.prototype.bind = (original => function (...args) {
+            return TokenHUD_bind.call(this, original.bind(this), ...args);
+        })(TokenHUD.prototype.bind);
+
+        TokenHUD.prototype._onToggleTarget = TokenHUD_onToggleTarget;
+    }
 
     function onToggleTarget(event) {
         event.preventDefault();
@@ -44,16 +68,16 @@ Hooks.once("ready", () => {
 
         targetControl.classList.toggle("active", hud[TARGET_CONTROL_ACTIVE]);
         targetControl.insertAdjacentHTML("beforeend", `
-            <div class="manage-player-targets player-list ${hud[TARGET_CONTROL_ACTIVE] ? "active" : ""}">
+            <div class="${MODULE_ID} player-list ${hud[TARGET_CONTROL_ACTIVE] ? "active" : ""}">
                 ${users.map(user => `
-                <div class="manage-player-targets player ${user.targeting ? "targeting" : ""} flexrow" data-user-id="${user.id}">
-                    <span class="manage-player-targets player-pip" style="background: ${user.color}; border-color: ${user.border}"></span>
-                    <span class="manage-player-targets player-name">${user.name}</span>
+                <div class="${MODULE_ID} player ${user.targeting ? "targeting" : ""} flexrow" data-user-id="${user.id}">
+                    <span class="${MODULE_ID} player-pip" style="background: ${user.color}; border-color: ${user.border}"></span>
+                    <span class="${MODULE_ID} player-name">${user.name}</span>
                 </div>`).join("")}
             </div>
         `);
 
-        const playerList = targetControl.querySelector(`.manage-player-targets.player-list`);
+        const playerList = targetControl.querySelector(`.${MODULE_ID}.player-list`);
 
         ["click", "contextmenu", "mouseenter", "mouseleave"].forEach(
             eventType => playerList.addEventListener(eventType, event => {
@@ -62,7 +86,7 @@ Hooks.once("ready", () => {
             })
         );
 
-        playerList.querySelectorAll(`.manage-player-targets.player`).forEach(
+        playerList.querySelectorAll(`.${MODULE_ID}.player`).forEach(
             element => element.addEventListener("click", onToggleTarget.bind(hud))
         );
     });

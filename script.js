8fb256d9a272bdffe072e56ca20054875e71ef85
dev7355608 <dev7355608@gmail.com>
2024-11-29 16:21:56 +0100

    Update for V13

--- script.js
+++ script.js
@@ -1,157 +1,127 @@
-Hooks.once("init", () => {
-    function setup() {
-        if (game.settings.get("core", "noCanvas")) {
-            return;
+Hooks.once("setup", () => {
+    if (game.settings.get("core", "noCanvas")) {
+        return;
+    }
+
+    game.socket.on("module.manage-player-targets", ({ sceneId, tokenIds, mode }) => {
+        if (sceneId === canvas.scene?.id) {
+            canvas.tokens.setTargets(tokenIds, { mode });
         }
+    });
 
-        game.socket.on("module.manage-player-targets", ({ sceneId, tokenIds, targeted }) => {
-            if (sceneId !== canvas.scene?.id) {
-                return;
-            }
+    if (!game.user.isGM) {
+        return;
+    }
 
-            for (const id of tokenIds) {
-                const token = canvas.tokens.get(id);
-
-                token?.setTarget(targeted, { releaseOthers: false, groupSelection: true });
-            }
-
-            game.user.broadcastActivity({ targets: game.user.targets.ids });
+    CONFIG.Token.hudClass = class extends CONFIG.Token.hudClass {
+        /** @override */
+        static DEFAULT_OPTIONS = foundry.utils.mergeObject(super.DEFAULT_OPTIONS, {
+            actions: {
+                "target": this.#onToggleTargetTray,
+                "manage-player-targets--target": this.#onToggleTarget,
+            },
         });
 
-        if (!game.user.isGM) {
-            return;
+        /** @type {boolean} */
+        #targetTrayActive = false;
+
+        /** @override */
+        bind(object) {
+            this.#targetTrayActive = false;
+
+            return super.bind(object);
         }
 
-        if (foundry.utils.isNewerVersion(game.version, 12)) {
-            CONFIG.Token.hudClass = class extends CONFIG.Token.hudClass {
-                /** @type {boolean} */
-                #targetTrayActive = false;
+        /** @override */
+        async _renderHTML(context, options) {
+            const result = await super._renderHTML(context, options);
 
-                /** @override */
-                bind(object) {
-                    this.#targetTrayActive = false;
-
-                    return super.bind(object);
-                }
-
-                /** @override */
-                activateListeners(html) {
-                    super.activateListeners(html);
-
-                    const targetControl = html[0].querySelector(`.control-icon[data-action="target"]`);
-
-                    targetControl.classList.toggle("active", this.#targetTrayActive);
-                }
-
-                /** @override */
-                _onClickControl(event) {
-                    const button = event.currentTarget;
-
-                    if (button.dataset.action === "target") {
-                        event.preventDefault();
-
-                        const active = this.#targetTrayActive = !this.#targetTrayActive;
-
-                        button.classList.toggle("active", active);
-                        button.querySelector(`.manage-player-targets.player-list`).classList.toggle("active", active);
-
-                        return;
-                    }
-
-                    super._onClickControl(event);
-                }
-            };
-        } else {
-            const targetTrayActive = Symbol("targetTrayActive");
-
-            TokenHUD.prototype.bind = (wrapped => function (object) {
-                this[targetTrayActive] = false;
-
-                return wrapped.call(this, object);
-            })(TokenHUD.prototype.bind);
-
-            TokenHUD.prototype.activateListeners = (wrapped => function (html) {
-                wrapped.call(this, html);
-
-                const targetControl = html[0].querySelector(`.control-icon[data-action="target"]`);
-
-                targetControl.classList.toggle("active", this[targetTrayActive]);
-            })(TokenHUD.prototype.activateListeners);
-
-            TokenHUD.prototype._onToggleTarget = function (event) {
-                event.preventDefault();
-
-                const button = event.currentTarget;
-                const active = this[targetTrayActive] = !this[targetTrayActive];
-
-                button.classList.toggle("active", active);
-                button.querySelector(`.manage-player-targets.player-list`).classList.toggle("active", active);
-            };
-        }
-
-        function onToggleTarget(event) {
-            event.preventDefault();
-
-            const token = this.object;
-            const userId = event.currentTarget.dataset.userId;
-            const targets = token.controlled ? token.layer.controlled : [token, ...token.layer.controlled];
-            const targeted = event.currentTarget.classList.toggle("targeting");
-
-            if (userId === game.user.id) {
-                targets.forEach(t => t.setTarget(targeted, { releaseOthers: false, groupSelection: true }));
-                game.user.broadcastActivity({ targets: game.user.targets.ids });
-            } else {
-                game.socket.emit("module.manage-player-targets", {
-                    sceneId: canvas.scene.id,
-                    tokenIds: targets.map(t => t.document.id),
-                    targeted
-                }, { recipients: [userId] });
-            }
-        };
-
-        Hooks.on("renderTokenHUD", (hud, html) => {
-            const users = game.users.filter(user => user.isSelf || user.active && user.viewedScene === canvas.scene?.id).map(user => ({
+            const users = game.users.filter((user) => user.isSelf || user.active && user.viewedScene === canvas.scene?.id).map((user) => ({
                 id: user.id,
                 name: user.name,
-                color: foundry.utils.Color.from(user.color).css,
-                border: foundry.utils.Color.from(user.border).css,
-                targeting: user.targets.has(hud.object)
+                color: user.color.css,
+                border: user.border.css,
+                targeting: user.targets.has(this.object),
             })).sort((a, b) => a.name.localeCompare(b.name, game.i18n.lang));
 
-            const targetControl = html[0].querySelector(`.control-icon[data-action="target"]`);
-
-            targetControl.insertAdjacentHTML("beforeend", `
-                <div class="manage-player-targets player-list">
-                    ${users.map(user => `
-                    <div class="manage-player-targets player ${user.targeting ? "targeting" : ""} flexrow" data-user-id="${user.id}">
-                        <span class="manage-player-targets player-pip" style="background: ${user.color}; border-color: ${user.border}"></span>
-                        <span class="manage-player-targets player-name">${user.name}</span>
-                    </div>`).join("")}
+            result.hud.querySelector(`.control-icon[data-action="target"]`).insertAdjacentHTML("afterend", `
+                <div class="manage-player-targets--player-list">
+                    ${users.map((user) => `
+                    <button type="button" class="manage-player-targets--player ${user.targeting ? "active" : ""}"
+                        style="--player-color: ${user.color}; --player-border: ${user.border}"
+                        data-action="manage-player-targets--target" data-user-id="${user.id}">
+                        <span class="manage-player-targets--player-name ellipsis">${Handlebars.Utils.escapeExpression(user.name)}</span>
+                    </button>`).join("")}
                 </div>
             `);
 
-            const playerList = targetControl.querySelector(`.manage-player-targets.player-list`);
+            return result;
+        }
 
-            ["click", "contextmenu", "mouseenter", "mouseleave"].forEach(
-                eventType => playerList.addEventListener(eventType, event => {
-                    event.preventDefault();
-                    event.stopPropagation();
-                })
-            );
+        /** @override */
+        async _onRender(context, options) {
+            await super._onRender(context, options);
 
-            playerList.querySelectorAll(`.manage-player-targets.player`).forEach(
-                element => element.addEventListener("click", onToggleTarget.bind(hud))
-            );
-        });
-    }
+            this.#toggleTargetTray(this.#targetTrayActive);
+        }
 
-    if (foundry.utils.isNewerVersion(game.version, 11)) {
-        Hooks.once("setup", setup);
-    } else {
-        Hooks.once("setup", () => {
-            if (!game.settings.get("core", "noCanvas")) {
-                Hooks.once("canvasInit", setup);
+        /** @override */
+        toggleStatusTray(active) {
+            super.toggleStatusTray(active);
+
+            active ??= this.element.querySelector(".status-effects").classList.contains("active");
+
+            if (active) {
+                this.#toggleTargetTray(false);
             }
-        });
-    }
+        }
+
+        /**
+         * Toggle the expanded state of the target selection tray.
+         * @param {boolean} [active] - Force the status tray to be active or inactive.
+         */
+        #toggleTargetTray(active) {
+            this.#targetTrayActive = active ??= !this.#targetTrayActive;
+
+            const target = this.element.querySelector(`.control-icon[data-action="target"]`);
+            const playerList = this.element.querySelector(`.manage-player-targets--player-list`);
+
+            target.classList.toggle("active", active);
+            playerList.classList.toggle("active", active);
+
+            if (active) {
+                playerList.style.top = `${target.offsetTop + target.offsetHeight / 2 - playerList.offsetHeight / 2}px`;
+
+                this.toggleStatusTray(false);
+            }
+        }
+
+        /**
+         * @this
+         * @param {PointerEvent} event
+         * @param {HTMLButtonElement} target
+         */
+        static #onToggleTargetTray(event, target) {
+            this.#toggleTargetTray(!this.#targetTrayActive);
+        }
+
+        /**
+         * @this
+         * @param {PointerEvent} event
+         * @param {HTMLButtonElement} target
+         */
+        static #onToggleTarget(event, target) {
+            const token = this.object;
+            const tokens = token.controlled ? token.layer.controlled : [token, ...token.layer.controlled];
+            const tokenIds = tokens.map((token) => token.document.id);
+            const mode = target.classList.toggle("active") ? "acquire" : "release";
+            const userId = target.dataset.userId;
+
+            if (userId === game.user.id) {
+                canvas.tokens.setTargets(tokenIds, { mode });
+            } else {
+                game.socket.emit("module.manage-player-targets", { sceneId: canvas.scene.id, tokenIds, mode }, { recipients: [userId] });
+            }
+        }
+    };
 });
